c47e6485e2e9158ade2efa0b6693c12b7f867acb
itchytummy <broanmol17@gmail.com>
2024-04-02 14:01:16 +0530

    ok

--- DQLfrozenlake.py
+++ DQLfrozenlake.py
@@ -20,6 +20,20 @@ class DQN(nn.Module):
         x = F.relu(self.fc1(x)) # Apply rectified linear unit (ReLU) activation
         x = self.out(x)         # Calculate output
         return x
+    # class DQN(nn.Module):
+
+    # def __init__(self, n_observations, n_actions):
+    #     super(DQN, self).__init__()
+    #     self.layer1 = nn.Linear(n_observations, 128)
+    #     self.layer2 = nn.Linear(128, 128)
+    #     self.layer3 = nn.Linear(128, n_actions)
+
+    # # Called with either one element to determine next action, or a batch
+    # # during optimization. Returns tensor([[left0exp,right0exp]...]).
+    # def forward(self, x):
+    #     x = F.relu(self.layer1(x))
+    #     x = F.relu(self.layer2(x))
+    #     return self.layer3(x)
 
 # Define memory for Experience Replay
 class ReplayMemory():
